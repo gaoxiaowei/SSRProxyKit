74b51ed4fe9ad883534374841258bd05d8908fd5
gaoxiaowei <gaoxiaowei@360.cn>
2020-05-25 18:58:07 +0800

    again

--- SSRProxyKit/SSRProxyKit/SSRProxyKit.h
+++ SSRProxyKit/SSRProxyKit/SSRProxyKit.h
@@ -16,5 +16,6 @@ FOUNDATION_EXPORT const unsigned char SSRProxyKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SSRProxyKit/PublicHeader.h>
 
-#import <SSRProxyKit/Potatso.h>
 #import <SSRProxyKit/SSRProxyManager.h>
+#import <SSRProxyKit/Potatso.h>
+#import <SSRProxyKit/JSONUtils.h>
